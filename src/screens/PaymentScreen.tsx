91024daf46422634653ec4512ccd3dcb0301a813
mateosofka <mateo.gutierrez@sofka.com.co>
2022-11-25 08:01:22 -0500

    feat: payment screen

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,10 +1,22 @@
 import {Text, View} from 'react-native';
 import React from 'react';
-
+import {MainButton} from '../components/atoms/MainButton';
+import {FormInput} from '../components/molecules/FormInput';
+import useCurrency from '../hooks/UseCurrency';
+import {styles} from '../themes/WalletTheme';
 export const PaymentScreen = () => {
+  const {currencyFormat} = useCurrency();
+  const balance: number = 642345678;
   return (
-    <View>
-      <Text>Payment Screen</Text>
+    <View style={styles.main}>
+      <View style={styles.titlesContainer}>
+        <Text style={styles.h1}>{currencyFormat(balance)}</Text>
+        <Text style={styles.h3}>Account balance</Text>
+      </View>
+      <FormInput icon="person" placeholder="User’s email or phone number" />
+      <FormInput icon="credit-card" placeholder="Amount" />
+      <FormInput icon="message" placeholder="Reason" />
+      <MainButton text="Send payment" width={92} />
     </View>
   );
 };
--- src/themes/WalletTheme.tsx
+++ src/themes/WalletTheme.tsx
@@ -2,9 +2,28 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   main: {
+    flex: 1,
     height: '100%',
     flexDirection: 'column',
     backgroundColor: 'white',
   },
   listContainer: {marginTop: 10},
+  titlesContainer: {
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 28,
+  },
+  h1: {
+    fontFamily: 'Roboto',
+    fontWeight: '400',
+    fontSize: 36,
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+
+  h3: {
+    fontFamily: 'Roboto',
+    fontWeight: '600',
+    fontSize: 22,
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
 });
